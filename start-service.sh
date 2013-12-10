5895deef79825eb719017b0acc53d607e411da48
Totto <totto@totto.org>
2013-12-10 12:40:16 +0100

    Refactored names of propertyfiles to be conform with itself and the rest of the whydah modules

--- start-service.sh
+++ start-service.sh
@@ -10,7 +10,7 @@ JARFILE=$A-$V.jar
 pkill -f $A
 
 wget --user=altran --password=l1nkSys -O $JARFILE "http://mvnrepo.cantara.no/service/local/artifact/maven/content?r=altran-snapshots&g=net.whydah.sso.service&a=$A&v=$V&p=jar"
-nohup java -jar -DIAM_CONFIG=useradmin.TEST.properties $JARFILE &
+nohup java -jar -DIAM_CONFIG=useradministration.TEST.properties $JARFILE &
 
 tail -f nohup.out
 
