c7b73517e22b94f4e19e0cec1fbeec200257ede5
maurtvedt <maurtvedt@gmail.com>
2014-08-29 12:53:11 +0200

    Update myUserDataStore.js

--- src/main/resources/js/myApp/myUserDataStore.js
+++ src/main/resources/js/myApp/myUserDataStore.js
@@ -1,5 +1,5 @@
 
 var myUserDataStore = new Ext.data.SimpleStore({
           fields: ['username', 'firstName'],
-          data : [['roy.tore.ostensen@obos.no','Roy Tore'],['thor.henning.hetland@obos.no','Thor Henning'],['eyvind.holand@obos.no','Eyvind']]
+          data : [['thor.henning.hetland@altran.com','Thor Henning']]
       });
