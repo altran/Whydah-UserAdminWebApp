60b07666c4a87cc53086a8914b8275d42d9bd78d
Totto <totto@totto.org>
2014-09-22 14:03:22 +0200

    completed cleaning up the name for UAWA

--- start-service.sh
+++ start-service.sh
@@ -3,14 +3,14 @@
 export IAM_MODE=DEV
 #export IAM_MODE=TEST
 
-A=UserAdminWebapp
+A=UserAdminWebApp
 V=LATEST
 JARFILE=$A-$V.jar
 
 pkill -f $A
 
-wget --user=altran --password=l1nkSys -O $JARFILE "http://mvnrepo.cantara.no/service/local/artifact/maven/content?r=snapshots&g=net.whydah.identity&a=$A&v=$V&p=jar"
-nohup java -jar -DIAM_CONFIG=useradministration.TEST.properties $JARFILE &
+wget  $JARFILE "http://mvnrepo.cantara.no/service/local/artifact/maven/content?r=snapshots&g=net.whydah.identity&a=$A&v=$V&p=jar"
+nohup java -jar -DIAM_CONFIG=useradminwebapp.TEST.properties $JARFILE &
 
 tail -f nohup.out
 
