19334e3c30519a866f0d75f5540c126ba44cb5c4
Totto <totto@totto.org>
2014-11-23 16:21:10 +0100

    property to control the ssl certificate verification

--- src/main/java/net/whydah/identity/ServerRunner.java
+++ src/main/java/net/whydah/identity/ServerRunner.java
@@ -1,15 +1,26 @@
 package net.whydah.identity;
 
+import net.whydah.identity.admin.config.AppConfig;
+import net.whydah.identity.admin.config.SSLTool;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.springframework.web.servlet.DispatcherServlet;
+
+import java.util.Properties;
 
 public class ServerRunner {
     public static final int PORT_NO = 9996;
     //public static final String TESTURL = "http://localhost:" + PORT_NO + "/action";
 
 	public static void main(String[] arguments) throws Exception {
+		// Property-overwrite of SSL verification to support weak ssl certificates
+		Properties properties = AppConfig.readProperties();
+		if ("disabled".equalsIgnoreCase(properties.getProperty("sslverification"))) {
+			SSLTool.disableCertificateValidation();
+
+		}
+
 		Server server = new Server(PORT_NO);
 		ServletContextHandler context = new ServletContextHandler(server, "/useradmin");
 
