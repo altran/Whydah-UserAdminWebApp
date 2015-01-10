79d1866050619c6fb6ce5666e901e298fb637f3c
Totto <totto@totto.org>
2015-01-10 20:37:23 +0100

    Refactored to have start and update scripts aligned with the rest of the whydah components

--- start-service.sh
+++ start-service.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
 
-export IAM_MODE=DEV
-#export IAM_MODE=TEST
-
-A=UserAdminWebApp
-V=LATEST
-JARFILE=$A-$V.jar
-
-pkill -f $A
-
-wget  $JARFILE "http://mvnrepo.cantara.no/service/local/artifact/maven/content?r=snapshots&g=net.whydah.identity&a=$A&v=$V&p=jar"
-nohup java -jar -DIAM_CONFIG=useradminwebapp.TEST.properties $JARFILE &
-
-tail -f nohup.out
-
+nohup /usr/bin/java -DIAM_MODE=PROD -DIAM_CONFIG=/home/UserAdminWebApp/useradminwebapp.PROD.properties -jar /home/UserAdminWebApp/UserAdminWebApp.jar &
